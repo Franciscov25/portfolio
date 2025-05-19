e3a4c702885c032f8a169ec05a975460a4429ec0
Francisco <franciscovargasmarcal@gmail.com>
2025-05-19 19:19:16 -0300

    Feat: ajustando a estrutura dos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from "./Components/Header"
+import Footer from "./Components/Footer"
+import Home from "./routes/Home"
+import Sobre from "./routes/Sobre"
+import Contato from "./routes/Contato"
+import Projetos from "./routes/Projetos"
 
 function App() {
 
   return (
     <>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="*" element={<Error/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/sobre" element={<Sobre/>}/>
+        <Route path="/contato" element={<Contato/>}/>
+        <Route path="/projetos" element={<Projetos/>}/>
+      </Routes>
+      <Footer/>
+    </Router>
     </>
   )
 }
