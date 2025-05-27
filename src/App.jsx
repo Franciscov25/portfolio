75ad8f1b3c29694a1a194a502761617a44167ef6
Francisco <franciscovargasmarcal@gmail.com>
2025-05-27 16:44:56 -0300

    Fix: correção na chamada das rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
@@ -8,19 +7,16 @@ import Contato from "./routes/Contato"
 import Projetos from "./routes/Projetos"
 import DetalhesProjeto from "./Components/DetalhesProjeto"
 import ParticlesBackground from "./Components/ParticlesBackground"
-import Error from "./routes/Error" // Certifique-se de importar o componente Error
+import Error from "./routes/Error" 
+import SimpleSpotifyEmbed from "./Components/SimpleSpotifyEmbed"
 
 function App() {
   return (
-    // O container principal do App terá flex-col e min-h-screen para preencher a viewport.
-    // O bg-zinc-900 aqui atua como um fundo geral.
     <div className="flex flex-col min-h-screen bg-zinc-900">
       <Router>
         <Header/>
-        <ParticlesBackground /> {/* Particles continua fixo e no z-index -1 */}
+        <ParticlesBackground /> 
 
-        {/* O main com flex-grow vai preencher o espaço entre Header e Footer.
-            Ele não precisa de z-index, pois o z-index dos componentes de rota cuidará da sobreposição. */}
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home/>}/>
@@ -31,6 +27,7 @@ function App() {
             <Route path="*" element={<Error/>}/>
           </Routes>
         </main>
+        <SimpleSpotifyEmbed/>
         <Footer/>
       </Router>
     </div>
