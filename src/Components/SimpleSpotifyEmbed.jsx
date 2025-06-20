cfac0a479d9278cd7d921aade4775ecce6931823
Francisco <franciscovargasmarcal@gmail.com>
2025-06-20 16:34:47 -0300

    Feat: adicionando sessão spotify com respectiva api para exibir na home

--- src/Components/SimpleSpotifyEmbed.jsx
+++ src/Components/SimpleSpotifyEmbed.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 
 const SimpleSpotifyEmbed = () => {
-  // Substitua este iframe pelo código que você copiou do Spotify
-  const spotifyEmbedCode = `
-    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/2ZRo7axmMPeSVUvDbGkJah?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-  `;
-
   return (
-    <div className="bg-zinc-800 rounded-lg p-5 mx-auto my-10 max-w-2xl shadow-lg text-white">
-      <h2 className="text-2xl font-bold mb-6 text-center text-green-400">
-        Minha Playlist Favorita
-      </h2>
-      {/* dangerouslySetInnerHTML é necessário para renderizar HTML puro em React */}
-      <div dangerouslySetInnerHTML={{ __html: spotifyEmbedCode }} />
-      <p className="text-sm text-gray-400 mt-4 text-center">
-        Curta a playlist diretamente do Spotify!
-      </p>
+    <div className='p-7'>
+      <div className="border-2 border-blue-400 bg-gradient-to-br from-zinc-900 to-blue-950 bg-zinc-800 rounded-lg p-5 mx-auto my-10 max-w-2xl shadow-lg text-white relative z-10">
+        <h2 className="text-2xl font-bold mb-6 text-center text-blue-400">
+          Minha Música Favorita
+        </h2>
+
+        <iframe
+          className="rounded-xl w-full h-[352px] z-10 relative"
+          src="https://open.spotify.com/embed/track/1BncfTJAWxrsxyT9culBrj?utm_source=generator&theme=0"
+          frameBorder="0"
+          allowFullScreen
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></iframe>
+
+        <p className="text-sm text-blue-300 mt-4 text-center">
+          Curta a playlist diretamente do Spotify!
+        </p>
+      </div>
     </div>
   );
 };
